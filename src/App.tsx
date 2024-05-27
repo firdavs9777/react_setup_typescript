d4ac762d2f668ea1cf1106d6d83c050958ea1b12
firdavs9777 <fmutalipov97@gmail.com>
2024-05-27 15:16:41 +0900

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,4 @@
-// import React, { useState } from 'react';
-// import TodoList from './pages/TodoList';
-// import NewToDo from './pages/NewToDo';
-// import { ToDo } from './todo.model';
 
-// const App: React.FC = () => {
-//   // const todos = [{ id: 'id1', text: 'Finish the course' }];
-//   const [todos, setTodos] = useState<ToDo[]>( [])
-  
-//   const todoHandler = (text: string) => {
-//      // Managing state better  =>  using spread operator  and update the value
-//      setTodos([...todos, { id: Math.random().toString(), text: text }])
-//   }
-//   const todoDeleteHandler = (todoId: string) => {
-//     setTodos(prevTodos => {
-//       return prevTodos.filter(todo => todo.id !== todoId)
-//     })
-//   }
-
-
-//   // function component returns jsx
-//   return (
-//     <div>
-//       <NewToDo onAddToDo={todoHandler} />
-//       <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
-//     </div>
-  
-//   )
-// }
-// // Working with props and types for props
-
-// export default App;
 import CourseGoal from './components/CourseGoal';
 import Header from "./pages/HeaderField";
 import goalsImg from './assets/goal.jpg';
@@ -74,7 +43,6 @@ export default function App() {
   }
   function handleDeleteGoal(goalId: number) {
         setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== goalId));
-
   }
   return (
     <main>
